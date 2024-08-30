dc3fd09b10ef61137c260c83c235b40e57f8472f
Onmeje TG <tonmeje@gmail.com>
2024-08-30 18:33:29 +0100

    Day 001a

--- day001/task.py
+++ day001/task.py
@@ -1,4 +1,4 @@
 print("Welcome to the Band Name generator")
-city = input("What is your city\n")
-pet = input("what is the name of your pet\n")
+city = input("What is your city?\n")
+pet = input("what is the name of your pet?\n")
 print(f"Your Band name is {city} {pet}")
